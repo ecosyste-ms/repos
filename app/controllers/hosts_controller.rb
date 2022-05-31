a252d3c964800b09045d1509adca84b16e73f85f
Andrew Nesbitt <andrewnez@gmail.com>
2022-05-31 17:21:23 +0100

    Order repos by most recently synced

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,6 +1,6 @@
 class HostsController < ApplicationController
   def show
     @host = Host.find_by_name(params[:id])
-    @pagy, @repositories = pagy(@host.repositories)
+    @pagy, @repositories = pagy(@host.repositories.order('last_synced_at DESC'))
   end
 end
