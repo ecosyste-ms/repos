8d20a91e8bc9a854389914d5c5dc3e9aca13c52a
Andrew Nesbitt <andrewnez@gmail.com>
2022-06-17 17:24:55 +0100

    Rename some things

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Packages
+module Repositories
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
