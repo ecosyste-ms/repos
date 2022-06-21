cb1f727f11bbe3883e3d6b8788944d8e49eaf43a
Andrew Nesbitt <andrewnez@gmail.com>
2022-06-21 15:04:56 +0100

    faster loading of repos in api controllers

--- app/controllers/api/v1/manifests_controller.rb
+++ app/controllers/api/v1/manifests_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::ManifestsController < Api::V1::ApplicationController
   def index
     @host = Host.find_by_name(params[:host_id])
-    @repository = @host.repositories.find_by_full_name(params[:repository_id])
+
+    @repository = @host.repositories.find_by('lower(full_name) = ?', params[:repository_id].downcase)
     @pagy, @manifests = pagy(@repository.manifests.includes(:dependencies))
   end
 end
