95a060b507a983b587e3980ba804af0e5d3d8d68
Andrew Nesbitt <andrewnez@gmail.com>
2022-07-07 15:01:53 +0100

    typo

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -18,7 +18,7 @@ class Repository < ApplicationRecord
   scope :without_manifests, -> { includes(:manifests).where(manifests: {repository_id: nil}) }
 
   def self.parse_dependencies_async
-    Repository.not(dependency_job_id: nil).limit(1000).each(&:parse_dependencies_async)
+    Repository.where.not(dependency_job_id: nil).limit(1000).each(&:parse_dependencies_async)
     Repository.where('last_synced_at > ?', 1.week.ago).where(fork: false).where(dependencies_parsed_at: nil, dependency_job_id: nil).limit(1000).each(&:parse_dependencies_async)
   end
 
