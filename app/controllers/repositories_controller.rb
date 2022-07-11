9beed0d5c44f88a550e15908f21c0bd4fab14096
Andrew Nesbitt <andrewnez@gmail.com>
2022-07-11 15:08:56 +0100

    Raise 404 instead of 500 on missing repo pages

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,6 +2,7 @@ class RepositoriesController < ApplicationController
   def show
     @host = Host.find_by_name(params[:host_id])
     @repository = @host.repositories.find_by('lower(full_name) = ?', params[:id].downcase)
+    raise ActiveRecord::RecordNotFound if @repository.nil?
     @manifests = @repository.manifests.includes(:dependencies).order('kind DESC')
   end
 end
