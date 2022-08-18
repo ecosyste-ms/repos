7e07b2cbd5277e3daf4ce3f9814e0b8c8b5853e2
Andrew Nesbitt <andrewnez@gmail.com>
2022-08-18 14:46:50 +0100

    Sync repositories after lookup

--- app/controllers/api/v1/repositories_controller.rb
+++ app/controllers/api/v1/repositories_controller.rb
@@ -38,7 +38,12 @@ class Api::V1::RepositoriesController < Api::V1::ApplicationController
 
   def show
     @host = Host.find_by_name!(params[:host_id])
-    @repository = @host.repositories.find_by!('lower(full_name) = ?', params[:id].downcase)
+    @repository = @host.repositories.find_by('lower(full_name) = ?', params[:id].downcase)
+    if @repository
+      render :show
+    else
+      render json: { error: 'Repository not found' }, status: :not_found
+    end
   end
 
   def lookup
@@ -47,7 +52,13 @@ class Api::V1::RepositoriesController < Api::V1::ApplicationController
     @host = Host.find_by_domain(parsed_url.host)
     raise ActiveRecord::RecordNotFound unless @host
     path = parsed_url.path.delete_prefix('/').chomp('/')
-    @repository = @host.repositories.find_by!('lower(full_name) = ?', path.downcase)
-    render :show
+    @repository = @host.repositories.find_by('lower(full_name) = ?', path.downcase)
+    if @repository
+      @repository.sync_async unless @repository.last_synced_at.present? && @repository.last_synced_at > 1.day.ago
+      render :show
+    else
+      @host.sync_repository_async(path)
+      render json: { error: 'Repository not found' }, status: :not_found
+    end
   end
 end
