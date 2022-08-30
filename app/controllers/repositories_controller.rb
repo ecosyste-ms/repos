57a4d61858c0eb8dc3f898390ad888f911759506
Andrew Nesbitt <andrewnez@gmail.com>
2022-08-30 09:45:21 +0100

    Attempt to sync missing repos

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,9 +1,14 @@
 class RepositoriesController < ApplicationController
   def show
     @host = Host.find_by_name!(params[:host_id])
-    @repository = @host.repositories.find_by!('lower(full_name) = ?', params[:id].downcase)
-    @manifests = @repository.manifests.includes(:dependencies).order('kind DESC')
-    @tags = @repository.tags.order('published_at DESC')
-    @sha = params[:sha] || @repository.default_branch
+    @repository = @host.repositories.find_by('lower(full_name) = ?', params[:id].downcase)
+    if @repository.nil?
+      @host.sync_repository_async(params[:id])
+      raise ActiveRecord::RecordNotFound
+    else
+      @manifests = @repository.manifests.includes(:dependencies).order('kind DESC')
+      @tags = @repository.tags.order('published_at DESC')
+      @sha = params[:sha] || @repository.default_branch
+    end
   end
 end
