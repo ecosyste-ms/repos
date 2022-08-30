ecae1dcea123132094d062a97f3468a1b74eaa1c
Andrew Nesbitt <andrewnez@gmail.com>
2022-08-30 10:02:56 +0100

    Custom error responses

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module Repositories
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.exceptions_app = routes
   end
 end
