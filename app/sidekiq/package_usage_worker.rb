4f73540ab941088802a2d890ec37d09108e9aee4
Andrew Nesbitt <andrewnez@gmail.com>
2022-09-29 16:22:24 +0100

    eager loading

--- app/sidekiq/package_usage_worker.rb
+++ app/sidekiq/package_usage_worker.rb
@@ -2,7 +2,7 @@ class PackageUsageWorker
   include Sidekiq::Worker
 
   def perform(repository_id)
-    r = Repository.find_by_id(repository_id)
+    r = Repository.includes(manifests: :dependencies).find_by_id(repository_id)
     PackageUsage.aggregate_dependencies_for_repo(r) if r
   end
 end
