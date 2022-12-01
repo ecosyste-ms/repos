c8d27418d3e4d5cc30145d0cfb4b098d94b8935d
Andrew Nesbitt <andrewnez@gmail.com>
2022-12-01 15:16:55 +0000

    Sort tags by published_at

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::TagsController < Api::V1::ApplicationController
   def index
     @host = Host.find_by_name!(params[:host_id])
     @repository = @host.repositories.find_by!('lower(full_name) = ?', params[:repository_id].downcase)
-    @pagy, @tags = pagy(@repository.tags)
+    @pagy, @tags = pagy(@repository.tags.order('published_at DESC'))
   end
 end
