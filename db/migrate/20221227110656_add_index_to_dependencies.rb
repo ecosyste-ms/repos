affde3cc7b4d598ca0c15a55378447ef8242efd7
Andrew Nesbitt <andrewnez@gmail.com>
2022-12-27 11:12:25 +0000

    Add index to dependencies table

---
+++ db/migrate/20221227110656_add_index_to_dependencies.rb
@@ -0,0 +1,5 @@
+class AddIndexToDependencies < ActiveRecord::Migration[7.0]
+  def change
+    add_index :dependencies, [:ecosystem, :package_name]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_112519) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_27_110656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_02_112519) do
     t.boolean "direct", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["ecosystem", "package_name"], name: "index_dependencies_on_ecosystem_and_package_name"
     t.index ["manifest_id"], name: "index_dependencies_on_manifest_id"
   end
 
