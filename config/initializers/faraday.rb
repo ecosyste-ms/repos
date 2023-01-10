a67e964fada482e91b2cac846a1f5e2f16538879
Andrew Nesbitt <andrewnez@gmail.com>
2023-01-10 19:33:55 +0000

    Configure octokit to use typhoeus

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,2 +1,10 @@
 require 'faraday/typhoeus'
 Faraday.default_adapter = :typhoeus
+
+Octokit.middleware = Faraday::RackBuilder.new do |builder|
+  builder.use Octokit::Middleware::FollowRedirects
+  builder.use Octokit::Response::RaiseError
+  builder.request :instrumentation
+  builder.request :retry
+  builder.adapter Faraday.default_adapter, accept_encoding: "gzip"
+end
