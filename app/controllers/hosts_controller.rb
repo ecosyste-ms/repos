d8f86b853945f6c2371d43717d1b257bd689888b
Andrew Nesbitt <andrewnez@gmail.com>
2023-02-15 16:44:36 +0000

    Add dropdown to sort repos

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,6 +1,16 @@
 class HostsController < ApplicationController
   def show
     @host = Host.find_by_name!(params[:id])
-    @pagy, @repositories = pagy_countless(@host.repositories.where.not(last_synced_at:nil).order('last_synced_at desc'))
+
+    scope = @host.repositories.where.not(last_synced_at:nil)
+
+    sort = params[:sort].presence || 'updated_at'
+    if params[:order] == 'asc'
+      scope = scope.order(Arel.sql(sort).asc.nulls_last)
+    else
+      scope = scope.order(Arel.sql(sort).desc.nulls_last)
+    end
+
+    @pagy, @repositories = pagy_countless(scope)
   end
 end
