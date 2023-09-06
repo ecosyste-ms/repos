c699c89ad77c3653ad43e6256c16b90bbe4ae235
Andrew Nesbitt <andrewnez@gmail.com>
2023-09-06 14:29:23 +0100

    Sort dependencies api by id

--- app/controllers/api/v1/dependencies_controller.rb
+++ app/controllers/api/v1/dependencies_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DependenciesController < Api::V1::ApplicationController
   def index
     @usage = PackageUsage.find_by(ecosystem: params[:ecosystem], name: params[:name])
     raise ActiveRecord::RecordNotFound unless @usage
-    @scope = Dependency.where(ecosystem: @usage.ecosystem, package_name: @usage.name).includes(:repository, :manifest)
+    @scope = Dependency.where(ecosystem: @usage.ecosystem, package_name: @usage.name).includes(:repository, :manifest).order('id asc')
     @scope = @scope.where('id > ?', params[:after]) if params[:after].present?
     @pagy, @dependencies = pagy(@scope)
   end
