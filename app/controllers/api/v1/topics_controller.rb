d369cc8b959de3d329fb4a7248e9b3bfbb6068d2
Andrew Nesbitt <andrewnez@gmail.com>
2023-09-13 11:51:00 +0100

    Allow sorting repo in topics api endpoint

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -11,6 +11,17 @@ class Api::V1::TopicsController < Api::V1::ApplicationController
     scope = Repository.topic(@topic).includes(:host)
 
     @related_topics = (scope.pluck(:topics).flatten - [@topic]).inject(Hash.new(0)) { |h, e| h[e] += 1; h }.sort_by { |_, v| -v }.first(100)
+
+    scope = scope.created_after(params[:created_after]) if params[:created_after].present?
+    scope = scope.updated_after(params[:updated_after]) if params[:updated_after].present?
+
+    if params[:sort].present? || params[:order].present?
+      sort = params[:sort] || 'last_synced_at'
+      order = params[:order] || 'desc'
+      sort_options = sort.split(',').zip(order.split(',')).to_h
+      scope = scope.order(sort_options)
+    end
+
     @pagy, @repositories = pagy(scope)
   end
 end
