a778f2ad0d67ca6221120f1789385068fe9106dd
Andrew Nesbitt <andrewnez@gmail.com>
2023-09-23 17:59:21 +0100

    put extra details jobs in their own queue

--- app/sidekiq/sync_extra_details_worker.rb
+++ app/sidekiq/sync_extra_details_worker.rb
@@ -1,6 +1,6 @@
 class SyncExtraDetailsWorker
   include Sidekiq::Worker
-  sidekiq_options lock: :until_executed, queue: 'tags'
+  sidekiq_options lock: :until_executed, queue: 'extra'
 
   def perform(repository_id)
     Repository.find_by_id(repository_id).try(:sync_extra_details)
