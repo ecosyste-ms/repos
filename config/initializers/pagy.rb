14d520bae36521ca7477fc9e3412849d5ae41ce3
Andrew Nesbitt <andrewnez@gmail.com>
2023-10-12 16:24:55 +0100

    return empty page on overflow instead of error

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -3,7 +3,9 @@ require 'pagy/extras/items'
 require 'pagy/extras/countless'
 require 'pagy/extras/bootstrap'
 require 'pagy/extras/array'
+require 'pagy/extras/overflow'
 
 Pagy::DEFAULT[:items] = 100
 Pagy::DEFAULT[:items_param] = :per_page
 Pagy::DEFAULT[:max_items] = 1000
+Pagy::DEFAULT[:overflow] = :empty_page
