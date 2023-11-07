c35f5ecb834594c48b0dc4af30dcf7d2be0abc1b
Andrew Nesbitt <andrewnez@gmail.com>
2023-11-07 16:26:11 +0000

    Update tag counts

--- app/models/hosts/gitea.rb
+++ app/models/hosts/gitea.rb
@@ -79,7 +79,7 @@ module Hosts
           published_at: tag['commit']['created']
         })
       end
-      repository.update_columns(tags_last_synced_at: Time.now)
+      repository.update_columns(tags_last_synced_at: Time.now, tags_count: repository.tags.count)
     rescue *IGNORABLE_EXCEPTIONS
       nil
     end
