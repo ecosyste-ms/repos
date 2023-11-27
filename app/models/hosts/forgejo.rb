58cb45bb8cce070c7a3f89de26ab5bff8876a2a1
Andrew Nesbitt <andrewnez@gmail.com>
2023-11-27 15:06:04 +0000

    Add support for forgejo #395

---
+++ app/models/hosts/forgejo.rb
@@ -0,0 +1,15 @@
+module Hosts
+  class Forgejo < Gitea
+    
+    def icon
+      'go-forgejo'
+    end
+    
+    def api_client
+      Faraday.new(@host.url, request: {timeout: 30}) do |conn|
+        conn.request :authorization, :bearer, REDIS.get("forgejo_token:#{@host.id}")
+        conn.response :json        
+      end
+    end
+  end
+end
