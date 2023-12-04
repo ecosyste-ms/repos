c38cf543089e48cfd7c0d36b97b17df94fd4b2ea
Andrew Nesbitt <andrewnez@gmail.com>
2023-12-04 08:25:40 +0000

    Only count hosts with at least one repo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @hosts = Host.all.order('repositories_count DESC')
+    @hosts = Host.all.order('repositories_count DESC').where('repositories_count > 0')
   end
 end
