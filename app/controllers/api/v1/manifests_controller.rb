4f5504e77f7bd73158446f400f44f8a1ff516c60
Andrew Nesbitt <andrewnez@gmail.com>
2024-03-23 10:33:54 +0000

    Add fresh_when method call to improve caching

--- app/controllers/api/v1/manifests_controller.rb
+++ app/controllers/api/v1/manifests_controller.rb
@@ -4,6 +4,8 @@ class Api::V1::ManifestsController < Api::V1::ApplicationController
 
     @repository = @host.repositories.find_by!('lower(full_name) = ?', params[:repository_id].downcase)
     
+    fresh_when(@repository, public: true)
+
     if params[:tag_id].present?
       @tag = @repository.tags.find_by_name!(params[:tag_id])
       @pagy, @manifests = pagy(@tag.manifests.includes(:dependencies))
