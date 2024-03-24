53b7342ca4c84e7d50834c7f1aad3cee09ca741c
Andrew Nesbitt <andrewnez@gmail.com>
2024-03-24 21:07:28 +0000

    Add skip_before_action and after_action in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  skip_before_action :verify_authenticity_token
+
+  after_action lambda {
+    request.session_options[:skip] = true
+  }
 end
