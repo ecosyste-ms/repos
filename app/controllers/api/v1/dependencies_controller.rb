64289c0c335d99df52a2e8e2a2f6636033bd8306
Andrew Nesbitt <andrewnez@gmail.com>
2024-03-25 14:17:38 +0000

    Fix ambiguous column name in dependencies api controller

--- app/controllers/api/v1/dependencies_controller.rb
+++ app/controllers/api/v1/dependencies_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::DependenciesController < Api::V1::ApplicationController
   def index
     @usage = PackageUsage.find_by(ecosystem: params[:ecosystem], name: params[:name])
     raise ActiveRecord::RecordNotFound unless @usage
-    @scope = Dependency.where(ecosystem: @usage.ecosystem, package_name: @usage.name).includes(:repository, :manifest).order('id asc')
-    @scope = @scope.where('id > ?', params[:after]) if params[:after].present?
+    @scope = Dependency.where(ecosystem: @usage.ecosystem, package_name: @usage.name).includes(:repository, :manifest).order('dependencies.id asc')
+    @scope = @scope.where('dependencies.id > ?', params[:after]) if params[:after].present?
     @pagy, @dependencies = pagy(@scope)
     fresh_when(@dependencies, public: true)
   end
