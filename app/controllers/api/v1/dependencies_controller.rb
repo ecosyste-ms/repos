5b44d72891d4c2689f13fbf08a223abe31ef0307
Andrew Nesbitt <andrewnez@gmail.com>
2024-04-05 10:47:02 +0100

    Remove order from dependency api controller

--- app/controllers/api/v1/dependencies_controller.rb
+++ app/controllers/api/v1/dependencies_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DependenciesController < Api::V1::ApplicationController
   def index
     @usage = PackageUsage.find_by(ecosystem: params[:ecosystem], name: params[:name])
     raise ActiveRecord::RecordNotFound unless @usage
-    @scope = Dependency.where(ecosystem: @usage.ecosystem, package_name: @usage.name).includes(:manifest, {repository: :host}).order('dependencies.id asc')
+    @scope = Dependency.where(ecosystem: @usage.ecosystem, package_name: @usage.name).includes(:manifest, {repository: :host})#.order('dependencies.id asc')
     @scope = @scope.where('dependencies.id > ?', params[:after]) if params[:after].present?
     @pagy, @dependencies = pagy_countless(@scope)
     fresh_when(@dependencies, public: true)
