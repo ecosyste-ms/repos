fdefa30af820eac81bfafc0efa534dd65d4d6cc5
Andrew Nesbitt <andrewnez@gmail.com>
2024-10-29 09:02:11 +0000

    typo

--- app/sidekiq/download_tags_worker.rb
+++ app/sidekiq/download_tags_worker.rb
@@ -1,6 +1,6 @@
 class DownloadTagsWorker
   include Sidekiq::Worker
-  sidekiq_options queue: 'tags', # lock: :until_executed
+  sidekiq_options queue: 'tags'# # lock: :until_executed
 
   def perform(repository_id)
     Repository.find_by_id(repository_id).try(:download_tags)
