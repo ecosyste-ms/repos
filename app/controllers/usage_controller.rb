03dfaa7875d3355ac4347a748371d267d9f7b182
Andrew Nesbitt <andrewnez@gmail.com>
2025-05-23 15:09:43 +0100

    Temporarily disable package usage pages due to scaling issues

--- app/controllers/usage_controller.rb
+++ app/controllers/usage_controller.rb
@@ -1,36 +1,37 @@
 class UsageController < ApplicationController
   def index
-    @ecosystems = PackageUsage.group(:ecosystem).count.sort_by{|e,c| -c }
-    expires_in 1.day, public: true
+    # @ecosystems = PackageUsage.group(:ecosystem).count.sort_by{|e,c| -c }
+    # expires_in 1.day, public: true
   end
 
   def ecosystem
     @ecosystem = params[:ecosystem]
-    @scope = PackageUsage.where(ecosystem: @ecosystem).select('ecosystem,name,dependents_count,package').order('dependents_count DESC')
-    @pagy, @package_usages = pagy_countless(@scope)
-    expires_in 1.day, public: true
+    # @scope = PackageUsage.where(ecosystem: @ecosystem).select('ecosystem,name,dependents_count,package').order('dependents_count DESC')
+    # @pagy, @package_usages = pagy_countless(@scope)
+    # expires_in 1.day, public: true
   end
 
   def show
     @package_usage = PackageUsage.find_by(ecosystem: params[:ecosystem], name: params[:name])
-    fresh_when @package_usage, public: true
-    if @package_usage.nil?
-      if Dependency.where(ecosystem: params[:ecosystem], package_name: params[:name]).any?
-        @package_usage = PackageUsage.create({
-          ecosystem: params[:ecosystem],
-          name: params[:name],
-          key: "#{params[:ecosystem]}:#{params[:name]}",
-          dependents_count: 1})
-        if @package_usage.persisted?
-          @package_usage.sync 
-        else
-          raise ActiveRecord::RecordNotFound
-        end
-      else
-        raise ActiveRecord::RecordNotFound
-      end
-    end
-    @scope = Dependency.where(ecosystem: @package_usage.ecosystem, package_name: @package_usage.name).includes(:manifest, {repository: :host})
-    @pagy, @dependencies = pagy_countless(@scope)
+    raise ActiveRecord::RecordNotFound if @package_usage.nil?
+    # fresh_when @package_usage, public: true
+    # if @package_usage.nil?
+    #   if Dependency.where(ecosystem: params[:ecosystem], package_name: params[:name]).any?
+    #     @package_usage = PackageUsage.create({
+    #       ecosystem: params[:ecosystem],
+    #       name: params[:name],
+    #       key: "#{params[:ecosystem]}:#{params[:name]}",
+    #       dependents_count: 1})
+    #     if @package_usage.persisted?
+    #       @package_usage.sync 
+    #     else
+    #       raise ActiveRecord::RecordNotFound
+    #     end
+    #   else
+    #     raise ActiveRecord::RecordNotFound
+    #   end
+    # end
+    # @scope = Dependency.where(ecosystem: @package_usage.ecosystem, package_name: @package_usage.name).includes(:manifest, {repository: :host})
+    # @pagy, @dependencies = pagy_countless(@scope)
   end
 end
