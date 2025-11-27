0b43dafb9e46ff253ef044ddb8277243e3bd2017
Andrew Nesbitt <andrewnez@gmail.com>
2025-11-27 09:29:48 +0000

    Remove timezone preservation setting in Active Support configuration

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,5 @@ module Repositories
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.exceptions_app = self.routes
-    config.active_support.to_time_preserves_timezone = :zone
   end
 end
