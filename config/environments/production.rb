b89a7fc3b0ae42810fd770000a8aa522ce7c1024
Andrew Nesbitt <andrewnez@gmail.com>
2025-11-27 09:00:09 +0000

    Enable STDOUT logging synchronization in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
+  STDOUT.sync = true
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
