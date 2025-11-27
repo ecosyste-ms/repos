a4832714e6e90a477fd8349f0020e7eb3d889329
Andrew Nesbitt <andrewnez@gmail.com>
2025-11-27 09:44:45 +0000

    Refactor sorting logic in HostsController#show; simplify scope handling and default sort order

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -6,17 +6,13 @@ class HostsController < ApplicationController
   def show
     @host = Host.find_by_name!(params[:id])
 
-    scope = @host.repositories#.where.not(last_synced_at:nil)
+    scope = @host.repositories
 
-    if params[:sort].present? || params[:order].present?
-      sort = params[:sort].presence || 'updated_at'
-      if params[:order] == 'asc'
-        scope = scope.order(Arel.sql(sort).asc.nulls_last)
-      else
-        scope = scope.order(Arel.sql(sort).desc.nulls_last)
-      end
+    sort = params[:sort].presence || 'id'
+    if params[:order] == 'asc'
+      scope = scope.order(Arel.sql(sort).asc.nulls_last)
     else
-      #scope = scope.order('updated_at desc')
+      scope = scope.order(Arel.sql(sort).desc.nulls_last)
     end
 
     @pagy, @repositories = pagy_countless(scope)
