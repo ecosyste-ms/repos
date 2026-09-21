8ae5f1c70af0510536698d5f9a8ea4489e8058fa
Andrew Nesbitt <andrewnez@gmail.com>
2026-09-21 15:05:17 +0100

    Skip dependencies with missing manifests in usage API

--- test/controllers/api/v1/dependencies_controller_test.rb
+++ test/controllers/api/v1/dependencies_controller_test.rb
@@ -65,4 +65,30 @@ class Api::V1::DependenciesControllerTest < ActionDispatch::IntegrationTest
     assert data.is_a?(Array)
     assert_equal [], data
   end
+
+  test "should skip dependencies with missing manifests" do
+    @package_usage.update!(name: 'litegraph.js')
+    @dependency.update!(package_name: 'litegraph.js')
+    missing_manifest = create(:manifest, repository: @repository)
+    create(:dependency, manifest: missing_manifest, repository: @repository,
+      ecosystem: 'npm', package_name: 'litegraph.js')
+    missing_manifest.delete
+
+    get '/api/v1/usage/npm/litegraph.js/dependencies'
+
+    assert_response :success
+    data = JSON.parse(@response.body)
+    assert_equal [@dependency.id], data.map { |dependency| dependency['id'] }
+    assert_equal 'litegraph.js', data.first['package_name']
+    assert_equal @manifest.filepath, data.first.dig('manifest', 'filepath')
+  end
+
+  test "should return an empty array when all dependencies have missing manifests" do
+    @manifest.delete
+
+    get api_v1_usage_dependencies_path(ecosystem: 'npm', name: 'react')
+
+    assert_response :success
+    assert_equal [], JSON.parse(@response.body)
+  end
 end
